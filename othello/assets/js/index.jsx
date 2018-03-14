0a55cfbcb5d272189b7d174d3db0c90b61276af4
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-13 23:38:43 -0400

    Fix the enter button in index.jsx

--- othello/assets/js/index.jsx
+++ othello/assets/js/index.jsx
@@ -1,6 +1,5 @@
 import React                                                          from 'react';
 import ReactDOM                                                       from 'react-dom';
-import { Button, Container, Row, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 import Logo                                                           from './components/logo'
 
 export default function run_index(root) {
@@ -34,16 +33,17 @@ class Index extends React.Component {
           <Logo />
           <h1>Welcome to <a target="_blank" href="https://en.wikipedia.org/wiki/Othello">Othello</a></h1>
         </header>
-        <Form>
-          <FormGroup>
+
+        <form onSubmit={function(e) {
+            e.preventDefault();
+            window.location=link;
+          }}>
             <div className="input">
-              <Input onChange={this.handleChange} placeholder={'Enter game name'}/>
+              <input onKeyUp={this.handleChange} placeholder={'Enter game name'}/>
+              <button id="enter" type="submit" color='success'>Join Game</button>
             </div>
-            <div className="join">
-              <Button id="enter" color='success' href={link}>Join Game</Button>
-            </div>
-          </FormGroup>
-        </Form>
+        </form>
+
         <footer>
           <p><a target="_blank" href="https://github.com/YuqingCheng/cs5610-project-1"><i className="fab fa-github"/> source code</a></p>
         </footer>
