d6f01c620fd9f0fbdd57b5a7ee2f3790fb1108b5
Yuan Gao <gaoyuanbj@yahoo.com>
2018-03-16 23:54:09 -0400

    Add Info part, to be discussed

---
+++ othello/assets/js/info.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+export default class Info extends React.Component {
+  // Userinfo
+  render() {
+    return (
+      <div>
+        <div className="userinfo">
+          <p>UserInfo: Show the two players name</p>
+        </div>
+        <div className="speculatorinfo">
+          <p>Speculator: Show current speculators name</p>
+        </div>
+        <div className="score">
+          <p>White * how many</p>
+          <p>Black * how many</p>
+        </div>
+        <div className="turn">
+          <p>Who's turn</p>
+        </div>
+        <div>
+          <div>
+            <button>Reset</button>
+          </div>
+          <div>
+            <button>Exit(May delete the game)</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- othello/assets/js/othello.jsx
+++ othello/assets/js/othello.jsx
@@ -4,6 +4,7 @@ import { Stage, Layer, Rect, Text, Circle }                           from 'reac
 import Konva                                                          from 'konva';
 import { Button, Container, Row, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 import Disc                                                           from './components/disc.jsx';
+import Info                                                           from "./info.jsx"
 
 export default function run_othello(root, channel) {
   ReactDOM.render(<Othello channel={channel}/>, root);
@@ -27,12 +28,12 @@ class Othello extends React.Component {
     this.loadImages.bind(this);
 
     this.loadImages();
-        
+
     channel.join()
       .receive("ok", resp => this.updateView(resp))
       .receive("error", resp => { console.log("Unable to join", resp); });
 
-    
+
 
     this.updateView.bind(this);
     this.move.bind(this);
@@ -44,15 +45,15 @@ class Othello extends React.Component {
     var images = {};
     var i = 0;
     images['black'] = new Image();
-    images['black'].onload = () => { 
+    images['black'].onload = () => {
                               if(i++ >= 1) this.setState({images: images});
                             }
     images['white'] = new Image();
-    images['white'].onload = () => { 
+    images['white'].onload = () => {
                               if(i++ >= 1) this.setState({images: images});
                             }
     images['black'].src = black;
-    images['white'].src = white;       
+    images['white'].src = white;
 
   }
 
@@ -84,22 +85,31 @@ class Othello extends React.Component {
   render() {
     // TODO
     let i = 0;
-    const discs = this.state.colors.map(color => { 
-                                          return (<Disc 
+    const discs = this.state.colors.map(color => {
+                                          return (<Disc
                                                     key={i}
                                                     color={color}
                                                     onClick={this.state.lock ? null : this.move}
                                                     parent={this}
-                                                    index={i++} 
+                                                    index={i++}
                                                     images={this.state.images}
                                                     />);
                                         });
     return (
-      <Stage width={400} height={400} fill={'green'}>
-        <Layer>
-          {discs}
-        </Layer>
-      </Stage>
+      <Container>
+        <Row>
+          <Col xs="12" lg="8">
+            <Stage width={400} height={400} fill={'green'}>
+              <Layer>
+                {discs}
+              </Layer>
+            </Stage>
+          </Col>
+          <Col xs="12" lg="4">
+            <Info />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
